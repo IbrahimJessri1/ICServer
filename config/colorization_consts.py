f1b0d2650a46bd335e9f61115995a7c0d3042280
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2023-08-13 18:21:16 +0300

    fixed naming

--- config/colorization_consts.py
+++ config/colorization_consts.py
@@ -1,8 +1,8 @@
 IMAGE_HEIGHT = {'rgb' :256 , 'lab' : 256}
 IMAGE_WIDTH = {'rgb' :256 , 'lab' : 256}
 SR_MODEL_PATH = 'resources/RealESRGAN/RealESRGAN_x4plus.pth'
-GEN_8_MODEL_PATH = 'resources/GEN-8/gen15'
-GEN_11_MODEL_PATH = 'resources/GEN-11/gen14'
-GEN_12_MODEL_PATH = 'resources/GEN-12/gen5'
-GEN_13_MODEL_PATH = 'resources/GEN-13/gen42.h5'
-GEN_14_MODEL_PATH = 'resources/GEN-14/gen31'
+GEN_8_MODEL_PATH = 'resources/GEN-8/gen8'
+GEN_11_MODEL_PATH = 'resources/GEN-11/gen11'
+GEN_12_MODEL_PATH = 'resources/GEN-12/gen12'
+GEN_13_MODEL_PATH = 'resources/GEN-13/gen13.h5'
+GEN_14_MODEL_PATH = 'resources/GEN-14/gen14'
